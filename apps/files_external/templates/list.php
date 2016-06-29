1403b838ec2d93e8025df1f9f4f83b9f85716d7b
davitol <dtoledo@solidgear.es>
2016-06-29 10:49:07 +0200

    add link to personal settings in external storage

--- apps/files_external/templates/list.php
+++ apps/files_external/templates/list.php
@@ -7,7 +7,7 @@
 <div id="emptycontent" class="hidden">
 	<div class="icon-external"></div>
 	<h2><?php p($l->t('No external storage configured')); ?></h2>
-	<p><?php p($l->t('You can add external storages in the personal settings')); ?></p>
+<p><?php p(print_unescaped($l->t('<a href="/index.php/settings/personal#external-storage" target="_blank" rel="noreferrer">You can add external storages in the personal settings</a>'))); ?></p>
 </div>
 
 <input type="hidden" name="dir" value="" id="dir">
